99bdb146ed877972e01933b026e4b59d1980ce58
Swastik Panda <swastiksuman@gmail.com>
2019-09-12 23:01:04 +0530

    full working app

--- src/App.js
+++ src/App.js
@@ -12,19 +12,25 @@ function App() {
   const grids = [];
   const [xSelected, setXSelected] = useState([]);
   const [ySelected, setYSelected] = useState([]);
+  const [gridValue, setGridValues] = useState(['', '', '', '', '', '', '', '', '']); 
   const [currentUser, setCurrentUser] = useState('x');
   const winningGroups = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '5', '9'], ['3', '5', '7'], ['1', '4', '7'], ['3', '6', '9']];
   const setGridValue = (i) => {
+    console.log(gridValue);
     if(!(xSelected.includes(i) || ySelected.includes(i))){
+      var tempGridValue = gridValue;
     if(currentUser == 'x'){
       xSelected.push(i);
       setXSelected(xSelected);
       setCurrentUser('y');
+      tempGridValue[i-1] = 'X';
     }else{
       ySelected.push(i);
       setYSelected(ySelected);
       setCurrentUser('x');
+      tempGridValue[i-1] = 'Y';
     }
+    setGridValues(tempGridValue);
     console.log(xSelected);
     console.log(ySelected);
     winningGroups.map((x) => {
@@ -53,15 +59,15 @@ function App() {
   return (
     <div className="App">
       <div className="game-board">
-        <GridBox value='1' onClick={setGridValue}/>
-        <GridBox value='2' onClick={setGridValue}/>
-        <GridBox value='3' onClick={setGridValue}/>
-        <GridBox value='4' onClick={setGridValue}/>
-        <GridBox value='5' onClick={setGridValue}/>
-        <GridBox value='6' onClick={setGridValue}/>
-        <GridBox value='7' onClick={setGridValue}/>
-        <GridBox value='8' onClick={setGridValue}/>
-        <GridBox value='9' onClick={setGridValue}/>
+        <GridBox value='1' status={gridValue[0]} onClick={setGridValue}/>
+        <GridBox value='2' status={gridValue[1]} onClick={setGridValue}/>
+        <GridBox value='3' status={gridValue[2]} onClick={setGridValue}/>
+        <GridBox value='4' status={gridValue[3]} onClick={setGridValue}/>
+        <GridBox value='5' status={gridValue[4]} onClick={setGridValue}/>
+        <GridBox value='6' status={gridValue[5]} onClick={setGridValue}/>
+        <GridBox value='7' status={gridValue[6]} onClick={setGridValue}/>
+        <GridBox value='8' status={gridValue[7]} onClick={setGridValue}/>
+        <GridBox value='9' status={gridValue[8]}onClick={setGridValue}/>
       </div>
     </div>
   );
